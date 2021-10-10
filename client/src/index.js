3cc857404b2b353178de838e7aadd3ad6ff084a9
msango1 <msangobiyi@gmail.com>
2021-10-10 22:28:17 +0100

    FEAT: cleanup BREAKING CHANGE: remove redundant App files FEAT: routing BREAKING CHANGE: create path switch

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Router, Switch } from 'react-router-dom'
+import Home from './Home';
+import AboutUs from './AboutUs';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about-us">
+          <AboutUs />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
