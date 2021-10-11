83f687d39dc43209f8f8100610355a2e9978423e
msango1 <msangobiyi@gmail.com>
2021-10-11 10:12:05 +0100

    FEAT: styling BREAKING CHANGE: page styling

--- client/src/Home.js
+++ client/src/Home.js
@@ -14,8 +14,13 @@ function Home() {
   return (
     
     <div className="container m-8">
-      <p>{backendData.header}</p>
-      <p>{backendData.body}</p>
+      <div className="bg-blue-100 p-4">
+        <p>Logo</p>
+      </div>
+      <div className="bg-white p-4">
+        <p className="font-sans text-3xl">{backendData.header}</p>
+        <p>{backendData.body}</p>
+      </div>
     </div>
   )
 }
